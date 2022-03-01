6379b2ac6f57f67347af4f023b5054c6acf89ced
madhu1199 <vmadhusudhan1104@gmail.com>
2022-03-01 15:30:30 +0530

    Binary Exponential Search

--- Exponential Search/src/com/mphasis/Exponential/ExponentialSearch.java
+++ Exponential Search/src/com/mphasis/Exponential/ExponentialSearch.java
@@ -1,10 +1,54 @@
 package com.mphasis.Exponential;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ExponentialSearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int arr[] = { 3, 6,6, 9, 12, 15, 17, 19 };
+		int length = arr.length;
 
+		System.out.println("Enter key for Exponential search");
+
+		Scanner scanner = new Scanner(System.in);
+		int key = scanner.nextInt();
+		
+		
+			int outcome = exponentialSearch(arr, length, key);
+
+			if(outcome < 0) {
+				
+				System.out.println("Element not found");
+				
+			}
+			else {
+				
+				
+				System.out.println("Element found at index "+outcome);
+			}
+			
+			
 	}
+	
+	
+	public static int  exponentialSearch(int arr[],int length , int key) {
+		
+		
+		if(arr[0]==key){
+            return 0;
+            }
+        int i=1;
+        while(i<length && arr[i]<=key){
 
+            i=i*2;
+            break;  // in case of repetation numbers if there is a break in the array list first element is printed
+        }               // if there is no break last element is printed
+
+        	return  Arrays.binarySearch(arr,i/2,Math.min(i, length), key);
+		
+	}
+	
 }
+	//	
