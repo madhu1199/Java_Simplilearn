3b934a06c83ea02e08c638e4295069d47e8520d6
madhu1199 <vmadhusudhan1104@gmail.com>
2022-04-09 15:43:08 +0530

    phase 2 practise

--- Validation of user Login/src/userlogin/Dashboard.java
+++ Validation of user Login/src/userlogin/Dashboard.java
@@ -1,11 +1,14 @@
 package userlogin;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Dashboard
@@ -26,9 +29,20 @@ public class Dashboard extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		response.setContentType("text/html");
+		HttpSession session = request.getSession();
+		PrintWriter out = response.getWriter();
+		// Check whether the session already exists
+		if ((session.getAttribute("userName") != null) &&
+		(session.getAttribute("userPassword") != null)) {
+		out.println("<h3>Welcome " + session.getAttribute("userName"));
+		out.println("<br> You are now logged in...</h3>");
+		out.println("<p><h4><a href='Logout'>LOGOUT</a></h4>");
+		}
+		else {
+		response.sendRedirect("index.html");
+		   }
+		 }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
